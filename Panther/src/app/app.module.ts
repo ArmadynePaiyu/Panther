8ed2c197a827c235aa430464f5bf8e9affd1d793
ArmadynePaiyu <nikhilradhakrishnan@yahoo.com>
2018-02-16 01:33:00 +0530

    Firebase installed

--- Panther/src/app/app.module.ts
+++ Panther/src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
+import {MaterializeModule} from 'angular2-materialize' ;
+import {AngularFireModule} from 'angularfire2';
+import {AngularFireDatabaseModule} from 'angularfire2/database';
+import {AngularFireAuthModule} from 'angularfire2/auth';
+import {RouterModule,Routes} from '@angular/router';
+import {FormsModule} from '@angular/forms';
 
+export const firebase = {
+  apiKey: "AIzaSyCI5BDcKoHYre2-YAVeVFPLrrMu_J_rDIQ",
+  authDomain: "blackpanther-6880b.firebaseapp.com",
+  databaseURL: "https://blackpanther-6880b.firebaseio.com",
+  projectId: "blackpanther-6880b",
+  storageBucket: "blackpanther-6880b.appspot.com",
+  messagingSenderId: "778400565752"
+
+}
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireAuthModule,
+    AngularFireDatabaseModule,
+    AngularFireModule.initializeApp(firebase),
+    MaterializeModule,
+    RouterModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
